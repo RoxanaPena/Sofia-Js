727b53fc824a9a15513d940eb2cbd46ccdf26ab4
RoxanaPena <roxa.97@hotmail.com>
2023-08-14 16:02:28 -0300

    cambios en el ciclo

--- main.js
+++ main.js
@@ -9,14 +9,14 @@ if (edad >= 18 ){
     alert("Hola " + nombre + " podes ingresar, recorda beber con moderacion.");
     console.log("La edad de " + nombre + " es " + edad);
 
-    let productoElejido = Number(prompt( "elije el producto que quiere comprar 1-Vino Blanco 2-Vino Tinto 3-Vino Rose"));
+    let productoElejido = Number(prompt( "Elije el producto que quiere comprar: 1-Vino Blanco 2-Vino Tinto 3-Vino Rose."));
     switch (productoElejido){
         case 1:
             let producto1 = 1600;
             let cantidad1 = Number(prompt( "El Vino Blanco tiene una valor de $1.600. Ingrese la cantidad de unidades que desea comprar."));
             let totalProducto1 = carrito(producto1, cantidad1);
             console.log( totalProducto1);
-            alert ("El total de tu compra es " + totalProducto1 + "¡Gracias por tu compra!");
+            alert ("El total de tu compra es " + totalProducto1 + " ¡Gracias por tu compra!");
 
             break;
     
@@ -25,7 +25,7 @@ if (edad >= 18 ){
             let cantidad2 = Number(prompt( "El Vino Tinto tiene una valor de $1.900. Ingrese la cantidad de unidades que desea comprar."));
             let totalProducto2 = carrito (producto2, cantidad2);
             console.log( totalProducto2 );
-            alert ("El total de tu compra es " + totalProducto2 + "¡Gracias por tu compra!");
+            alert ("El total de tu compra es " + totalProducto2 + " ¡Gracias por tu compra!");
 
             break;  
 
@@ -34,7 +34,7 @@ if (edad >= 18 ){
             let cantidad3 = Number(prompt( "El Vino Rose tiene una valor de $1.200. Ingrese la cantidad de unidades que desea comprar."));
             let totalProducto3 = carrito (producto3, cantidad3);
             console.log( totalProducto3);
-            alert ("El total de tu compra es " + totalProducto3 + "¡Gracias por tu compra!");
+            alert ("El total de tu compra es " + totalProducto3 + " ¡Gracias por tu compra!");
 
             break;
 
