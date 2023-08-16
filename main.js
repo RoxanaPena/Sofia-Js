f4a66f6ba1f35a3aedefcb8505778204ffb8fcf7
RoxanaPena <roxa.97@hotmail.com>
2023-08-15 23:04:07 -0300

    Agrego Objeto

--- main.js
+++ main.js
@@ -1,49 +1,11 @@
+//inicio
+
 let nombre  = prompt("Ingrese su nombre");
-
 let edad = prompt("Ingrese su edad");
-
-let producto = 1600;
-
 
 if (edad >= 18 ){
     alert("Hola " + nombre + " podes ingresar, recorda beber con moderacion.");
     console.log("La edad de " + nombre + " es " + edad);
-
-    let productoElejido = Number(prompt( "Elije el producto que quiere comprar: 1-Vino Blanco 2-Vino Tinto 3-Vino Rose."));
-    switch (productoElejido){
-        case 1:
-            let producto1 = 1600;
-            let cantidad1 = Number(prompt( "El Vino Blanco tiene una valor de $1.600. Ingrese la cantidad de unidades que desea comprar."));
-            let totalProducto1 = carrito(producto1, cantidad1);
-            console.log( totalProducto1);
-            alert ("El total de tu compra es " + totalProducto1 + " ¡Gracias por tu compra!");
-
-            break;
-    
-        case 2:
-            let producto2= 1900;
-            let cantidad2 = Number(prompt( "El Vino Tinto tiene una valor de $1.900. Ingrese la cantidad de unidades que desea comprar."));
-            let totalProducto2 = carrito (producto2, cantidad2);
-            console.log( totalProducto2 );
-            alert ("El total de tu compra es " + totalProducto2 + " ¡Gracias por tu compra!");
-
-            break;  
-
-        case 3:
-            let producto3= 1200;
-            let cantidad3 = Number(prompt( "El Vino Rose tiene una valor de $1.200. Ingrese la cantidad de unidades que desea comprar."));
-            let totalProducto3 = carrito (producto3, cantidad3);
-            console.log( totalProducto3);
-            alert ("El total de tu compra es " + totalProducto3 + " ¡Gracias por tu compra!");
-
-            break;
-
-        default:
-            console.log("No eligio ningun producto.");
-            alert("No eligio ningun producto.");
-    }
-
-
 
 }else {
     alert ("Hola " +nombre + " no podes Ingresar, sos menor de edad.");
@@ -51,9 +13,58 @@ if (edad >= 18 ){
 }
 
 
+//Productos
+
+class Producto {
+    constructor( nombre, tipo, precio, stock){
+        this.nombre = nombre
+        this.tipo = tipo
+        this.precio = precio
+        this.stock = stock
+    }
+}
+
+const prod1 = new Producto ( "Santa Julia Chenin Dulce Natural", "vino Blanco", 1600 , 1000 );
+const prod2 = new Producto ( "Familia Gascón Roble Malbec" , "Vino Tinto" , 2145 , 750 );
+const prod3 = new Producto ( "Aimé Rosé", "Vino Rose", 1408 , 1000 );
 
 
 //Calculo de compra
-function carrito( producto, cantidad){
-    return producto * cantidad;
+function carrito( producto , cantidad ){
+    const total = producto * cantidad ;
+    return total;
+}
+
+
+
+//cliente
+let seleccion = prompt (" Eliga un Producto:   "  + prod1.nombre + "    " + prod2.nombre + "     " + prod3.nombre);
+switch (seleccion){
+    case prod1.nombre:
+        let cantidad = Number( prompt ("¿Cuantas unidades desea comprar? "));
+        const total1 = carrito (prod1.precio , cantidad);
+        console.log ( nombre + " compro "+ cantidad + " "+ prod1.nombre + " El total de la compra es de " + total1 );
+        alert (nombre + " Compraste " + cantidad + " " + prod1.nombre + " El total es de " + total1 + "  ¡Gracias por tu compra!");
+
+        break;
+    
+    case prod2.nombre:
+        let cantidad2 = Number( prompt ("¿Cuantas unidades desea comprar? "));
+        const total2 = carrito (prod2.precio , cantidad2);
+        console.log ( nombre + " compro "+ cantidad2 + " "+ prod2.nombre + " El total de la compra es de " + total2 );
+        alert (nombre + " Compraste " + cantidad2 + " " + prod2.nombre + " El total es de " + total2 + "  ¡Gracias por tu compra!");
+
+        break;
+    
+    case prod3.nombre:
+        let cantidad3 = Number( prompt ("¿Cuantas unidades desea comprar? "));
+        const total3 = carrito (prod3.precio , cantidad3);
+        console.log ( nombre + " compro "+ cantidad3 + " "+ prod3.nombre + " El total de la compra es de " + total3 );
+        alert (nombre + " Compraste " + cantidad3 + " " + prod3.nombre + " El total es de " + total3 + "  ¡Gracias por tu compra!");
+
+        break;
+
+    default: 
+        alert ("No ingreso ningun producto, Intentelo nuevamente.");
+        console.log (nombre + " No ingreso ningun producto.");
 }
