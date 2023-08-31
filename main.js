3e01a55a8c9f2271ba7956e50231ef26f608b14f
RoxanaPena <roxa.97@hotmail.com>
2023-08-31 19:21:59 -0300

    modifico las funciones

--- main.js
+++ main.js
@@ -15,60 +15,149 @@ if ( edad >= 18 ){
 
 //Productos
 
-class Producto {
-    constructor( nombre, tipo, precio, stock ){
-        this.nombre = nombre
-        this.tipo = tipo
-        this.precio = precio
-        this.stock = stock
+const vinos = [
+    {
+        nombre : "Familia Gascón Roble Malbec",
+        precio : 1900,
+        stock : 1020,
+        tipo : "Tinto"
+    },
+    {
+        nombre : "Benjamín Nieto Malbec",
+        precio : 1444,
+        stock : 1500,
+        tipo : "Tinto"
+
+    },
+    {
+        nombre : "Trapiche Medalla Malbec",
+        precio : 4903,
+        stock : 7000,
+        tipo : "Tinto"
+    },
+    {
+        nombre : "Goye Cabernet Sauvignon",
+        precio : 1574,
+        stock : 2400,
+        tipo : "Tinto"
+    },
+    {
+        nombre : "Del Fin del Mundo Reserva Malbec",
+        precio : 2838,
+        stock : 3200,
+        tipo : "Tinto"
+    },
+    {
+        nombre : "Santa Julia Chenin Dulce Natural",
+        precio : 1900,
+        stock : 1000,
+        tipo : "Blanco"
+    },
+    {
+        nombre : "Goye Tocai",
+        precio : 1574,
+        stock : 3000,
+        tipo : "Blanco"
+    },
+    {
+        nombre : "Benjamín Nieto Colección Tardía Blanco",
+        precio : 1506,
+        stock : 500,
+        tipo : "Blanco"
+    },
+    {
+        nombre : "Emilia Nieto Senetiner Chardonnay",
+        precio : 1842,
+        stock : 1900,
+        tipo : "Blanco"
+    },
+    {
+        nombre : "La Linda Chardonnay",
+        precio : 3552,
+        stock : 6000,
+        tipo : "Blanco"
+    },
+    {
+        nombre : "Aimé Rosé",
+        precio : 1900,
+        stock : 1500,
+        tipo : "Rose"
+    },
+    {
+        nombre : "Goyenechea Merlot Rosé",
+        precio : 1574,
+        stock : 3000,
+        tipo : "Rose"
+    },
+    {
+        nombre : "Benjamín Nieto Colección Tardía Rosado",
+        precio : 1506,
+        stock : 2500,
+        tipo : "Rose"
+    },
+    {
+        nombre : "Lagarde Goes Pink Rosé",
+        precio : 2970,
+        stock : 1200,
+        tipo : "Rose"
+    },
+    {
+        nombre : "José L Mounier Rosado",
+        precio : 4455,
+        stock : 8000,
+        tipo : "Rose"
     }
-}
-
-const prod1 = new Producto ( "Santa Julia Chenin Dulce Natural", "vino Blanco", 1900 , 1000 );
-const prod2 = new Producto ( "Familia Gascón Roble Malbec" , "Vino Tinto" , 2145 , 750 );
-const prod3 = new Producto ( "Aimé Rosé", "Vino Rose", 1408 , 9000 );
-const prod4 = new Producto ( "Santa Julia Chenin Dulce Natural", "vino Blanco", 1600 , 10000 );
-const prod5 = new Producto ( "Familia Gascón Roble Malbec" , "Vino Tinto" , 2145 , 750 );
-const prod6 = new Producto ( "Aimé Rosé", "Vino Rose", 1408 , 9000 );
+];
 
 
 
-//Calculo de producto por cantidad elegida por el cliente
 
-function carrito ( producto , cantidad ){
+//funciones
+
+/* Por Tipo */
+const tinto = vinos.filter((producto) => producto.tipo === "Tinto");
+const blanco = vinos.filter((producto) => producto.tipo === "Blanco");
+const rose = vinos.filter((producto) => producto.tipo === "Rose");
+
+/*Por Nombre */
+const vinoTinto = tinto.map((producto) => producto.nombre );
+const vinoBlanco = blanco.map((producto) => producto.nombre );
+const vinoRose = rose.map((producto) => producto.nombre );
+
+/*Por Precio */
+const precioTinto = tinto.map((producto) => producto.precio );
+const precioBlanco = blanco.map((producto) => producto.precio );
+const precioRose = rose.map((producto) => producto.precio );
+
+/* Producto por cantidad*/
+function carrito( producto , cantidad ){
     const total = producto * cantidad ;
     return total;
 }
 
 
 
-//Ciclo donde se calcula lo que el cliente quiere comprar
+//Ciclo 
 
-let seleccion = parseInt(prompt( "Eliga un Producto:" + "\n 1- " + prod1.nombre + "\n 2- " +  prod2.nombre + "\n 3- " + prod3.nombre ));
+let seleccion = parseInt(prompt( "Seleccione el tipo de Vino que busca:" + " \n 1- Vino Tinto" + " \n 2- Vino Blanco" + " \n 3- Vino Rosé" ));
 
-switch ( seleccion ){
-    case 1:
-        let cantidad = parseInt(prompt( "¿Cuantas unidades desea comprar?" ));
-        const total1 = carrito ( prod1.precio , cantidad );
-        console.log ( nombre + " compro "+ cantidad + " "+ prod1.nombre + " El total de la compra es de " + total1 );
-        alert (nombre + " Compraste " + cantidad + " " + prod1.nombre + " El total es de " + total1 + "  ¡Gracias por tu compra!" );
-        break;
-    
-    case 2:
-        let cantidad2 = parseInt(prompt( "¿Cuantas unidades desea comprar?" ));
-        const total2 = carrito ( prod2.precio , cantidad2 );
-        console.log ( nombre + " compro "+ cantidad2 + " "+ prod2.nombre + " El total de la compra es de " + total2 );
-        alert (nombre + " Compraste " + cantidad2 + " " + prod2.nombre + " El total es de " + total2 + "  ¡Gracias por tu compra!" );
-        break;
-    
-    case 3:
-        let cantidad3 = parseInt(prompt( "¿Cuantas unidades desea comprar?" ));
-        const total3 = carrito ( prod3.precio , cantidad3 );
-        console.log ( nombre + " compro " + cantidad3 + " " + prod3.nombre + " El total de la compra es de " + total3 );
-        alert ( nombre + " Compraste " + cantidad3 + " " + prod3.nombre + " El total es de " + total3 + "  ¡Gracias por tu compra!" );
-        break;
+switch (seleccion) {
+    case 1: 
+    let producto1 = prompt( "Seleccione el producto: \n" + vinoTinto.join( "\n"));
+    console.log( producto1);
+    break;
 
-    default: 
-        alert (nombre + "No ingreso ningun producto, Intentelo nuevamente." );
-        console.log ( nombre + " No ingreso ningun producto." );
-}
+    case 2:  
+    let producto2 = parseInt(prompt( "Seleccione el producto: \n" + vinoBlanco.join( "\n")));
+    console.log( producto2);
+    break;
+    case 1: 
+
+    let producto3 = parseInt(prompt( "Seleccione el producto: \n " + vinoRose.join( "\n")));
+    console.log( producto3);
+    break;
+
+    default : 
+    alert ( "¡No ingreso un producto correcto, inentelo nuevamente!");
+    console.log( "No ingreso un producto correcto.");
+};
